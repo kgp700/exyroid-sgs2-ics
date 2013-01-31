298a7158b35682173d0daed8561e6f886cc72446
kgp700 <neokim20@gmail.com>
2013-02-01 01:04:50 +0900

    Revert: Fix Touchscreen Choppy for MXT224E Touchscreen controller

--- drivers/input/touchscreen/mxt224_u1.c
+++ drivers/input/touchscreen/mxt224_u1.c
@@ -199,7 +199,7 @@ struct mxt224_data {
 	bool enabled;
 };
 
-#undef CLEAR_MEDIAN_FILTER_ERROR
+#define CLEAR_MEDIAN_FILTER_ERROR
 struct mxt224_data *copy_data;
 int touch_is_pressed;
 EXPORT_SYMBOL(touch_is_pressed);
